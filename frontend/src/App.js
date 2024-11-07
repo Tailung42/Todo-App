6935835b5915094755c974568212aeea055b3055
Bipin Thapa <tailung12345678910@gmail.com>
2024-11-07 20:52:44 +0545

    create hello world frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const todoItems = [
+  {
+    id: 1,
+    title: "Design final project layout",
+    description: "Need to design the overall look   the webapp before writing code.",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Study",
+    description: "Read Algebra and History textbook for the upcoming test",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Sammy's books",
+    description: "Go to library to return Sammy's books",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Article",
+    description: "Write article on how to use Django with React",
+    completed: false,
+  },
+];
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      viewCompleted: false,
+      todoList: todoItems,
+    };
+  }
+
+  render() {
+    return (
+      <h1> Hello , world</h1>
+    )
+  }
 }
-
 export default App;
